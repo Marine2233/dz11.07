308e1ac1555d03030655484e296a05f96ae85a1a
Marine <marine_ponomarenko@mail.ru>
2023-07-17 09:15:32 +0300

    home work11.07

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// Напишите программу, которая принимает на вход 
+            // пятизначное число и проверяет, является ли оно 
+            // палиндромом.
+            // 14212 -> нет
+            // 12821 -> да
+            // 23432 -> да
+
+            // Console.WriteLine("Введите число");
+            // int number = Convert.ToInt32(Console.ReadLine());
+            // if (number >= 10000 && number <= 99999)
+            // {
+            //     int num = (number / 10000) % 10;
+            //     int num1 = (number / 1000) % 10;
+            //     int num2 = (number / 100) % 10;
+            //     int num3= (number /10) % 10;
+            //     int num4 =number % 10;  
+
+            //     if ( num == num4 && num1 == num3)
+            //     Console.WriteLine("Полиндром");
+
+            //     else
+            //     Console.WriteLine("Не полиндром");
+
+
+            // }
+        
+            // else
+            //  Console.WriteLine("число не пятизначное");
+
+
+
+            // задача2
+            // Напишите программу, которая принимает на вход 
+            // координаты двух точек и находит расстояние между ними
+            //  в 3D пространстве.
+            // A (3,6,8); B (2,1,-7), -> 15.84
+            // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+//             Console.Write("x1 = ");
+//                 var x1 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.Write("y1 = ");
+//                 var y1 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.Write("z1 = ");
+//                 var z1 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.Write("x2 = ");
+//                 var x2 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.Write("y2 = ");
+//                 var y2 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.Write("z2 = ");
+//                 var z2 = Convert.ToInt32(Console.ReadLine());
+
+//                 Console.WriteLine("Расстояние = " + Distance(x1, y1, z1, x2, y2, z2));
+
+            
+        
+// // возврвщ.растояние между 2- точками с ук.коор.
+//          double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+//                  {
+// //   return-возвращает результат             
+//   //извл.корня возв.в степ.   степень
+//             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+//                  }
+
+
+
+            // задача 3
+            // Напишите программу, которая принимает на вход число (N) и выдаёт 
+            // таблицу кубов чисел от 1 до N.
+            // 3 -> 1, 8, 27
+            // 5 -> 1, 8, 27, 64, 125
+
+            // Console.Write("Введите число: ");
+            //     int number = Convert.ToInt32(Console.ReadLine());
+
+            //     for (int i = 1; i <= number; i++)
+            //     {
+            //         var cub = Math.Pow(i,3);
+            //         Console.Write(cub + ", ");
+            //     }
